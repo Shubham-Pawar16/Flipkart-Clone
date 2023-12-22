a8a78f779b3f0bb3ca22db27853a2cb54040463a
Shubham Pawar <pawarshubham166@gmail.com>
2023-12-22 10:25:01 +0530

    Added

---
+++ Backend-Flipkart-Clone/src/main/java/com/example/BackendFlipkartClone/BackendFlipkartCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackendFlipkartClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFlipkartCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFlipkartCloneApplication.class, args);
+	}
+
+}
---
+++ Backend-Flipkart-Clone/src/test/java/com/example/BackendFlipkartClone/BackendFlipkartCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackendFlipkartClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFlipkartCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
